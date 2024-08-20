0285b734e672b73b46935d51d9ef86a12bcb0308
phyowaiyanlinzaw <phyowaiyanlinzaw@ucsy.edu.mm>
2024-08-20 20:44:17 +0700

    week1 codes done

---
+++ Week1/week1/Program.cs
@@ -0,0 +1,22 @@
+﻿//print out Hello World! to the console
+Console.WriteLine("Hello World!"); // Output: Hello World!
+
+//variable and data types
+int myNum = 5;               // Integer (whole number)
+double myDoubleNum = 5.99D;  // Floating point number
+char myLetter = 'D';         // Character
+bool myBool = true;          // Boolean
+string myText = "Hello";     // String
+float myFloat = 5.99F;       // Floating point number
+decimal myDecimal = 5.99M;   // Decimal
+
+Console.WriteLine("myNum: " + myNum); // Output: myNum: 5
+Console.WriteLine("myDoubleNum: " + myDoubleNum); // Output: myDoubleNum: 5.99
+Console.WriteLine("myLetter: " + myLetter); // Output: myLetter: D
+Console.WriteLine("myBool: " + myBool); // Output: myBool: True
+Console.WriteLine("myText: " + myText); // Output: myText: Hello
+Console.WriteLine("myFloat: " + myFloat); // Output: myFloat: 5.99
+Console.WriteLine("myDecimal: " + myDecimal); // Output: myDecimal: 5.99
+
+//to run the code , cd into the directory and run the following command
+//dotnet run
